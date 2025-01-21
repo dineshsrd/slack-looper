a5f35adfb676f8266cbc773b5ef13123992682ab
Dinesh Rajendran <dinesh.r@commerceiq.ai>
2025-01-21 15:04:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.listen(3000, () => {
 
 // using node-cron and git the endpoint every 30 seconds, write the code below
 
-cron.schedule('*/30 * * * * *', async () => {
-    await axios.get('https://slack-status-scheduler-app.onrender.com/health');
+cron.schedule('*/59 * * * * *', async () => {
+    await axios.get('https://slack-status-scheduler-app-i7qp.onrender.com/health');
 });
 
 cron.schedule('*/30 * * * *', async () => {
-    const response = await axios.get('https://slack-status-scheduler-app.onrender.com/health');
+    const response = await axios.get('https://slack-status-scheduler-app-i7qp.onrender.com/health');
     console.log(response.data);
 });
 
